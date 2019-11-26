32ca921fed922543845a94969927f74a8d62ae9f
fadiima92 <andrewfmadain@gmail.com>
2019-11-26 12:02:16 -0800

    commiting saves for the last few days. server is working. need to connect mongo.  user auth and passport should be good.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,10 @@ import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
 import "./App.css";
 import Footer from "./Components/Footer";
 import Home from "./containers/Home";
-import SymptomJournal from "./containers/SymptomJournal";
 import DoctorList from "./containers/DoctorsList";
 import Appointments from "./containers/Appointments";
 import MedLog from "./containers/MedLog";
 import Prescriptions from "./containers/Prescriptions";
-import Charts from "./containers/Charts";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NoMatch from "./containers/NoMatch";
@@ -34,56 +32,7 @@ class App extends Component {
     render() {
         return [
             <MuiThemeProvider theme={theme}>
-                <Router>
-                    <div>
-                        <Switch>
-                            <Route
-                                exact
-                                path="/"
-                                render={props => (
-                                    <Login
-                                        {...props}
-                                        setUser={this.setUser}
-                                    ></Login>
-                                )}
-                            />
-                            <Route
-                                exact
-                                path="/signup"
-                                component={Signup}
-                                loggedInUser={this.state.currentlyLoggedInUser}
-                            />
-                            <Route exact path="/home" component={Home} />
-                            <Route
-                                exact
-                                path="/symptoms"
-                                render={props => (
-                                    <SymptomJournal {...props}></SymptomJournal>
-                                )}
-                                loggedInUser={this.state.currentlyLoggedInUser}
-                            />
-                            <Route
-                                exact
-                                path="/doctors"
-                                component={DoctorList}
-                            />
-                            <Route
-                                exact
-                                path="/appointments"
-                                component={Appointments}
-                            />
-                            <Route exact path="/log" component={MedLog} />
-                            <Route
-                                exact
-                                path="/prescriptions"
-                                component={Prescriptions}
-                            />
-                            <Route exact path="/charts" component={Charts} />
-                            <Route component={NoMatch} />
-                        </Switch>
-                    </div>
-                </Router>
-                <Footer />
+
             </MuiThemeProvider>
         ];
     }
---
+++ models/token.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TokenSchema = new Schema({
+  value: { type: String, required: true },
+  userId: { type: String, required: true },
+  clientId: { type: String, required: true }
+});
+
+const Token = mongoose.model("Token", TokenSchema);
+
+module.exports = Token;
